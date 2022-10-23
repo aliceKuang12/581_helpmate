4f43db52b3122988748acaf913feef5f7e64b0ca
Drake Pham <DrakePham21@gmail.com>
2022-10-23 14:54:57 -0500

    connected Database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const data = require('./data');
+// const data = require('./data')
   
 const app = express();
 
 app.get('/testdata', (req, res, next) => {
 	console.log("Test data: ");
+  res.send('backend Data');
 })
 
 app.listen(5000, () => {
