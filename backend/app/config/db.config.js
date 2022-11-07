9147a946965ded44e1152486f0eaf3513aa760b6
Drake Pham <DrakePham21@gmail.com>
2022-11-06 22:48:08 -0600

    database access

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "database-1.cdronrai97vl.us-east-1.rds.amazonaws.com",
+    port:"3306",
+    user: "helpMate",
+    password: "Thai21200126.",
+    database:"helpMate"
+  };
---
+++ backend/app/models/db.js
@@ -0,0 +1,54 @@
+const mysql = require("mysql");
+const dbConfig = require("../config/db.config.js");
+
+const connection = mysql.createConnection({
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
+
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Data Connected!");
+    connection.end();
+});
+
+let queryState = "SELECT * from User where passWords= \'Drake21\'";
+let data;
+exports.infor = connection.query(queryState, function (error, results, fields) {
+    if (error) {
+        connection.destroy();
+        throw error;
+    } else {
+        // connected!
+        
+        data = JSON.parse(JSON.stringify(results))
+        return data;
+        // return data;
+        
+        // callback(error, results);
+        // connection.end(function (err) { callback(err, results); });
+    }
+});
+
+
+
+// exports.handler = (event, context, callback) => {
+
+//     connection.query('show tables', function (error, results, fields) {
+//         if (error) {
+//             connection.destroy();
+//             throw error;
+//         } else {
+//             // connected!
+//             console.log(results);
+//             callback(error, results);
+//             connection.end(function (err) { callback(err, results); });
+//         }
+//     });
+// };
+
+// exports.infor=data;
