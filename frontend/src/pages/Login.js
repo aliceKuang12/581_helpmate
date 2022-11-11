17213c01bd42a91b1304c1c583f8f8f877fa6714
Alice Kuang <yuchun.kuang@gmail.com>
2022-11-10 22:40:14 -0600

    Login stylized

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,4 @@
-import { Typography, Container, TextField, Box } from "@mui/material";
+import { Typography, Container, TextField, Box, Paper } from "@mui/material";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import Button from '@mui/material/Button';
@@ -10,25 +10,42 @@ const theme = createTheme({
 
 const Login = () => {
     return (
-        <Container>
-            <Box sx={{ marginY: 5, marginX: 23, marginTop: 15 }}>
-                <Typography variant="h4" component="h2">
-                    HelpMate Login
-                </Typography></Box>
-
-            <Box sx={{ marginY: 5, marginX: 25 }}>
-                <TextField id="filled-basic" label="Username" variant="filled" />
-                <br /> <br />
-                <TextField id="filled-basic" label="Password" variant="filled" />
-            </Box>
+        <Container className="LoginPage" sx={{width: 1300, height: 1000}}>
+            <br/><br/><br/>
+            <Paper elevation={9} sx={{ width: 600, height: 800}}>
             
-            <Box sx={{ marginTop: 10, marginX: 33 }}>
-                <Button variant="contained">Login</Button>
+            <Box >
+                <br/><br/> <br/><br/><br/>
+                <Typography variant="h3" component="h2" sx={{ marginX: 23 }}>
+                    HelpMate 
+                </Typography>
+                <Typography variant="body1" component="p" sx={{ marginY: 3, marginX: 27 }}>
+                    Here to help you!
+                </Typography>
             </Box>
-
-            <Box sx={{ marginY: 5, marginX: 28 }}>
-                <Button variant="contained">Create Account</Button>
+            <br/>
+            <Box sx={{ marginY: 5, marginX: 23 }}>
+                <TextField id="outlined-basic" label="Username" variant="filled" />
+                <br /> <br />
+                <TextField id="outlined-basic" label="Password" type="password" variant="filled" />
+                <Typography variant="caption" component="p" 
+                            sx={{marginLeft: 12, color: "blue", fontSize: 14}}
+                            onClick={() => {alert('User authentication, future sprint'); }}>
+                    Reset Password
+                </Typography>
+                <br/><br/><br/>
+                <Button variant="contained" label="Password" type="password"
+                         sx={{ marginLeft: 8 }}
+                         onClick={() => {alert('clicked'); }}>
+                    Login
+                </Button>
+                <br/>
+                <Button variant="contained" 
+                sx={{ marginTop: 8, marginLeft: 3 }}
+                        onClick={() => {alert('clicked'); }}>
+                Create Account</Button>
             </Box>
+            </Paper>
         </Container>
     );
 }
