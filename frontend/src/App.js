005b4d46731403ce283edd8ef1b9855280dd7a7c
Minh Huyen Nguyen <huyennm2002@gmail.com>
2022-11-11 11:21:26 -0600

    add sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Text from './components/Text'
-import TestClass from './components/TestClass'
-import UserName from './components/UserName'
-import {Button} from '@material-ui/core'; //importing material ui component
+import SignUp from './components/SignUp'
+// import Text from './components/Text'
+// import TestClass from './components/TestClass'
+// import UserName from './components/UserName'
+// import {Button} from '@material-ui/core'; //importing material ui component
 import Header from './NavBar.js';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="Background">
-      <div className="MenuBackground">
-          <Header/>
-      </div>
-      <table>
-        <tr>
-          <td>
-            <box>
-            &ensp;Top Alerts&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-            <space>&ensp;</space>
-            <box>
-            &ensp;Academics&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-            <space>&ensp;</space>
-            <box>
-            &ensp;Health&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-            <br></br>          
-          </td>
-        </tr>
-        <space>&ensp;</space>
-        <tr>
-          <td>
-          <box>
-            &ensp;Notes&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-            <space>&ensp;</space>
-            <box>
-            &ensp;Travel&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-            <space>&ensp;</space>
-            <box>
-            &ensp;Social&ensp;
-            <card>&ensp;new card&ensp;</card>
-            </box>
-          </td>
-        </tr>
-      </table>
-    </div>
+      <Router>
+        <Routes>
+          {/* <Route path = '/' element={}/> */}
+          <Route path = '/signup' element={<SignUp/>}/> 
+        </Routes>
+    </Router> 
   );
 }
 
---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,204 @@
+import React, {useState} from 'react';
+import TextField from '@mui/material/TextField';
+// import _isEmpty from 'lodash/isEmpty';
+import PropTypes from 'prop-types';
+import { Grid, Typography } from '@material-ui/core';
+import { Button } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import withStyles from '@mui/styles/withStyles';
+
+function SignUp() {
+  return (
+        <Grid container
+            justifyContent="center" 
+            alignItems="center" 
+            direction="column"
+            style={{minHeight:"100vh"}}
+        >
+            <Grid item>
+            <Typography variant="h5" color="primary">
+                Create a new user
+            </Typography>
+            </Grid>
+            <CreateForm/>
+        </Grid>
+    )
+}
+const styles = {
+    paper: {
+        padding: '1rem',
+    },
+}
+
+const CreateForm = (props) => {
+    const { classes } = props;
+    const [data, setData] = useState({
+        // fname: fname || '',
+        // lname: fname || '',
+        // username: email || '',
+        // email: email || '',
+        // password: password || '',
+        // phone: phone || '',
+        // address: address || '',
+        // dob: dob || '',
+        // avt: avt || null,
+    });
+
+    const {
+        username,
+        email,
+        fname,
+        lname,
+        password,
+        phone,
+        address,
+        avt,
+        dob,
+    } = data;
+
+    React.useEffect(() => {
+    setData({
+        fname: fname || '',
+        lname: fname || '',
+        username: email || '',
+        email: email || '',
+        password: password || '',
+        phone: phone || '',
+        address: address || '',
+        dob: dob || '',
+        avt: avt || null,
+    });
+    }, []);
+
+    const handleChange = (value, key) => {
+        setData(prevState => ({...prevState, [key]: value,}));
+    };
+
+  return (
+    <Grid 
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+    >
+        <Paper variant="outline" style={{padding:'2rem'}}>
+            <Grid item xs={12}>
+            <TextField 
+                id="email" 
+                label="Email" 
+                variant="outlined" 
+                onChange={e => handleChange(e.target.value, 'email')}
+                value={email}
+                fullWidth style={{marginBottom: '1rem'}}
+            />
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container spacing = {3}>
+                    <Grid item xs={6}>
+                        <TextField 
+                            id="fname" 
+                            label="First name" 
+                            variant="outlined"
+                            onChange={e => handleChange(e.target.value, 'fname')}
+                            fullWidth style={{marginBottom: '1rem'}}
+                            value={fname}
+                            placeholder="First name"
+                            />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField 
+                            id="lname" 
+                            label="Last name" 
+                            variant="outlined" 
+                            onChange={e => handleChange(e.target.value, 'lname')}
+                            value={lname}
+                            fullWidth style={{marginBottom: '1rem'}}/> 
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField 
+                    id="username" 
+                    label="Username" 
+                    variant="outlined" 
+                    onChange={e => handleChange(e.target.value, 'username')}
+                    value={username}
+                    fullWidth style={{marginBottom: '1rem'}}/>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container spacing = {3}>
+                    <Grid item xs={6}>
+                        <TextField 
+                            id="password" 
+                            label="Password" 
+                            variant="outlined"
+                            onChange={e => handleChange(e.target.value, 'password')}
+                            value={password}
+                            fullWidth style={{marginBottom: '1rem'}}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField 
+                            id="password-cf" 
+                            label="Password Confirmation" 
+                            variant="outlined"
+                            fullWidth style={{marginBottom: '1rem'}}/>
+                    </Grid>
+                </Grid>
+                
+            </Grid>
+            <Grid item xs={12}>
+                <TextField 
+                    id="phone" 
+                    label="Phone number" 
+                    variant="outlined"
+                    onChange={e => handleChange(e.target.value, 'phone')}
+                    value={phone}
+                    fullWidth style={{marginBottom: '1rem'}}/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField 
+                    id="dob" 
+                    label="Date of birth" 
+                    variant="outlined" 
+                    onChange={e => handleChange(e.target.value, 'dob')}
+                    value={dob}
+                    fullWidth style={{marginBottom: '1rem'}}/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField 
+                    id="address" 
+                    label="Address"
+                    variant="outlined"
+                    onChange={e => handleChange(e.target.value, 'address')}
+                    value={address}
+                    fullWidth style={{marginBottom: '1rem'}}/>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField 
+                    id="avt" 
+                    label="Profile Picture" 
+                    variant="outlined" 
+                    value={avt}
+                    fullWidth 
+                    style={{marginBottom: '1rem'}}
+                />
+            </Grid>
+            <Button 
+                text="Sign Up"
+                variant="outlined"
+                style={{marginBottom:'1em'}}
+            >
+                Sign Up
+            </Button>
+        </Paper>
+    </Grid>
+  )
+}
+
+CreateForm.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };  
+
+export default withStyles(styles)(SignUp);
+// export default (SignUp);
