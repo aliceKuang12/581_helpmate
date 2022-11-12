1a6118227096d5f8f0934be5bd8df0dd24b2b960
Eva Morrison <eva.morrison33@ku.edu>
2022-11-12 00:56:27 -0600

    update homepage format

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -5,11 +5,15 @@ import TestClass from '../components/TestClass'
 import UserName from '../components/UserName'
 import { Button, Typography } from '@material-ui/core'; //importing material ui component
 import Header from '../components/NavBar'
+import TourCard from '../components/TourCard'
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import modules from "./modules.json"
 
 const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
@@ -23,10 +27,27 @@ const Homepage = () => {
 
     return (
         <div className="Background">
-            <Header></Header>
-            
-           
-
+            <div className = "Header">
+                <Header></Header>
+            </div>
+            <div className = "body">
+                <Container sx = {{marginY: 5}}>
+                    {modules.map((Modules) => (
+                        <Typography
+                        variant = "h4"
+                        component = "h2"
+                        marginTop = {5}
+                        marginBottom = {3}>
+                            {Modules.name} 
+                        </Typography>,
+                        <Grid container spacing = {5}>
+                        {Modules.sections.map((section, index) => (
+                            <TourCard tour={section} key = {index}></TourCard>
+                        ))}
+                        </Grid>
+                    ))}
+                </Container>
+            </div>
         </div>
     );
 }
