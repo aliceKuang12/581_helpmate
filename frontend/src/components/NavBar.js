642e7f6894d465008393173a2e03be80d6a14460
Eva Morrison <eva.morrison33@ku.edu>
2022-11-11 22:45:43 -0600

    update the colors of the top bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -36,7 +36,7 @@ function Header(){
       };
 
     return (
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: "yellow" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -97,15 +97,15 @@ function Header(){
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: 'black', display: 'block' }} 
               >
                 {page}
               </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <typography textAlign="left">Welcome, "Users name"     </typography>
+          <Box sx={{ color:'black', flexGrow: 0 }}>
+            <typography>Welcome, "Users name"     </typography>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
