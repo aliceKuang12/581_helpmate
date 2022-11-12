23270535683ed8e4761b35d602d30cd9149bf9b8
Alice Kuang <yuchun.kuang@gmail.com>
2022-11-12 14:57:00 -0600

    Add sx styling, remove button from signup

--- frontend/src/components/BasicForm.js
+++ frontend/src/components/BasicForm.js
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import SignUp from './SignUp';
-import Stack from '@mui/material/Stack';
-import { Grid, Typography } from '@mui/material';
 
 export default function BasicForm() {
   const [open, setOpen] = React.useState(false);
