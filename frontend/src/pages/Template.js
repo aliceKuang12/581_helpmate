915dc9f7427d44e545d2e33997a64288bbea3030
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-11-15 14:01:15 -0800

    Update Template.js

--- frontend/src/pages/Template.js
+++ frontend/src/pages/Template.js
@@ -1,6 +1,22 @@
+import ModuleCard from '../components/ModuleCard'
+import Container from '@mui/material/Container'
+import Grid from "@mui/material/Grid"
+import NavBar from '../components/NavBar'
+import '../App.css';
+import Box from '@mui/material/Box'
+import Paper from "@mui/material/Paper"
+
 const Template = () => {
     return(
-        <div>I'm the template!</div>
+        <div>
+            <NavBar></NavBar>
+            <Container> 
+                <Grid container spacing={6}>
+                    <ModuleCard/>
+                    <ModuleCard/>
+                </Grid>
+            </Container>
+        </div>
     );
 }
 
