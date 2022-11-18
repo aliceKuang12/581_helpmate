b92bd20b954aade6ddd93c7f943e1579b9c0a377
Eva Morrison <eva.morrison33@ku.edu>
2022-11-17 20:27:15 -0600

    add clock to module card

--- frontend/src/components/ModuleCard.js
+++ frontend/src/components/ModuleCard.js
@@ -3,6 +3,9 @@ import Grid from "@mui/material/Grid"
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
+import Clock from '../components/Clock'
+
+
 const ModuleCard = () => {
     return <Grid item xs={6}>
         <Paper>
@@ -13,12 +16,13 @@ const ModuleCard = () => {
                     alignItems: "left",
                     height: 500,
                     backgroundColor: 'lightblue',
-
+                    borderRadius: 1,
                 }}
             >
             <Typography varient='h4' component='h2'>
-                Today's Events:
+                Today's Events: 
             </Typography>
+            <Clock/>
             </Box>
         </Paper>
     </Grid>
