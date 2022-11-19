f00333af4a3a550bfc039f480585446e70b11125
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-11-19 10:13:39 -0800

    Update Template.js
    added a background

--- frontend/src/pages/Template.js
+++ frontend/src/pages/Template.js
@@ -1,4 +1,5 @@
 import ModuleCard from '../components/ModuleCard'
+import ModuleCard2 from '../components/ModuleCard2'
 import Container from '@mui/material/Container'
 import Grid from "@mui/material/Grid"
 import NavBar from '../components/NavBar'
@@ -6,17 +7,23 @@ import '../App.css';
 import Box from '@mui/material/Box'
 import Paper from "@mui/material/Paper"
 import Clock from '../components/Clock'
+import Image from '../BirdBackground.jpg'
 
 const Template = () => {
     return(
         <div>
-            <NavBar/>
-            <Clock sx = {{marginY: 5}}>
-            </Clock>
-            <Container > 
-                <Grid container spacing={6}>
-                    <ModuleCard/>
-                    <ModuleCard/>
+            <Container maxWidth="100" maxHeight="100" style={{ backgroundImage: `url(${Image})` }}> 
+                <NavBar></NavBar>
+                <Grid container spacing={12}>
+                    <Grid item xs={12}>
+                        <Clock/>
+                    </Grid>
+                    <Grid sx = {{margin: 5}} item xs={12}>
+                        <Grid container spacing={6}>
+                            <ModuleCard/>
+                            <ModuleCard2/>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Container>
         </div>
