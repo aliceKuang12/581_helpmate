01cf12ca0712f12ed99e7e8bac1ba0e34ffde311
Minh Huyen Nguyen <huyennm2002@gmail.com>
2022-11-20 15:52:24 -0600

    resolve conflict after merge

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,164 +1,5 @@
-<<<<<<< HEAD:frontend/src/NavBar.js
 import * as React from 'react';
 import axios from 'axios';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { ThemeProvider } from "@material-ui/core/styles";
-import customtheme from "./components/Theme";
-
-const pages = ['Home', 'Academics', 'Health','Social','Travel','Streaks'];
-const settings = [ 'Account', 'Logout'];
-
-function Header(){
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const [user, setUser] = React.useState("");
-
-    const getUser = () => {
-      axios.get('http://localhost:5000/', {
-      headers: { "Access-Control-Allow-Origin": "*"
-    }
-  })
-    .then(function (response) {
-        console.log(response.data);
-        setUser(response.data.name);
-  });
-
-    }
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-      };
-      const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-      };
-    
-      const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-      };
-    
-      const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-      };
-    React.useEffect(() => {getUser();}, [])
-    console.log(user);
-    return (
-      <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'black',
-              textDecoration: 'none',
-            }}
-          >
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-              {user}
-              {/* <Typography
-                value = {user}
-              </Typography> */}
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-            
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-    );
-}
-=======
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -175,9 +16,19 @@ import MenuItem from '@mui/material/MenuItem';
 
 const pages = ['Home', 'Academics', 'Health','Social','Travel','Streaks'];
 
-function Header() {
+export default function Header() {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [user, setUser] = React.useState('');
+    const getUser = () => {
+        axios.get('http://localhost:5000/', {
+        headers: { "Access-Control-Allow-Origin": "*"}
+    })
+      .then(function (response) {
+          console.log(response.data);
+          setUser(response.data.name);
+    });
+    }
 
     const handleOpenNavMenu = (event) => {
       setAnchorElNav(event.currentTarget);
@@ -200,6 +51,7 @@ function Header() {
       window.location.href = '/login';
     }
 
+    React.useEffect(() => {getUser();}, [])
     return (
       <AppBar position="static" sx={{ bgcolor: "yellow" }}>
       <Container maxWidth="xl">
@@ -271,7 +123,7 @@ function Header() {
               </Button>
             ))}
           </Box>
-          <Typography color="black" marginRight={3}>Welcome, "Users name"</Typography>
+          <Typography color="black" marginRight={3}>Welcome, {user}</Typography>
           <Box sx={{ color:'black', flexGrow: 0 }}>
             
             <Tooltip title="Open settings">
@@ -311,7 +163,5 @@ function Header() {
         </Toolbar>
       </Container>
     </AppBar>
-    );
+  );
 }
->>>>>>> 1525aadb7a432197178d2c4af1e39f1c38afa3e0:frontend/src/components/NavBar.js
-export default Header;
