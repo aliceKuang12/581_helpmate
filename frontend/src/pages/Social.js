068b2b0eb67a9cd7c074b6aeffbffb46f60c0125
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-12-01 12:48:03 -0600

    Add files via upload

---
+++ frontend/src/pages/Social.js
@@ -0,0 +1,27 @@
+import ModuleCard from '../components/SocialCard1'
+import ModuleCard2 from '../components/SocialCard2'
+import Grid from "@mui/material/Grid"
+import NavBar from '../components/NavBar'
+import '../App.css';
+import Clock from '../components/Clock'
+import Image from '../images/BirdBackground.jpg'
+
+const Template = () => {
+    return(
+        <div style={{ backgroundImage: `url(${Image})`, 
+        backgroundSize: "cover" }}>
+            <NavBar></NavBar>
+            <Clock/>         
+            <Grid container spacing={12}>
+                <Grid  item xs={12} marginX = {5} marginBottom = {5}>
+                    <Grid container spacing={6}>
+                        <ModuleCard />
+                        <ModuleCard2/>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default Template;
