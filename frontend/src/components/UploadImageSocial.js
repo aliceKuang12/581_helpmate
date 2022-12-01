03d89830e27f19abe2867a0e92d41019ef9b8494
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-12-01 12:50:48 -0600

    Add files via upload

---
+++ frontend/src/components/UploadImageSocial.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import UploadIcon from '@mui/icons-material/Upload';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Ticket from '../images/socialimage.jpg'
+
+export default function UploadImage() {
+  const [file, setFile] = useState("Invalid Image Source");
+  function saveUrl(e) {
+      console.log(e.target.files);
+      setFile(URL.createObjectURL(e.target.files[0]));
+  }
+  return (
+    <Grid container-spacing={2}  alignItems="center" justifyContent="center" >
+        <Grid item xs={10} sx={{mx: 5}}>
+           <img src={file} width="300" height="350" 
+           onError = {() => setFile(Ticket)}
+           />
+        </Grid>
+    <br/>
+    <Stack direction="row" alignItems="center" spacing={2}>
+      <Button variant="contained" component="label"
+      sx={{
+        backgroundColor: "darkgreen",
+        fontFamily: "arial",
+        marginLeft: 15
+      }}
+      >
+      <UploadIcon sx={{fontSize: "medium" }} /> &nbsp; Upload
+        <input hidden accept="image/*" 
+                multiple type="file" 
+                onChange={saveUrl}/>
+      </Button>
+    </Stack>
+    </Grid>
+  );
+}
---
+++ frontend/src/components/ViewEventsSocial.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+export default function BasicForm() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div >
+      <Button onClick={handleClickOpen} variant="outlined" sx={{backgroundColor: "cornsilk", fill: "blue", color:"Black" }}>
+        <CalendarMonthIcon sx={{fontSize: "large", color: "green"}}/>
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+            Connect to Gmail API, future sprint
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+      
+    </div>
+  );
+}
