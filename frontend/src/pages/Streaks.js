7216502420d32e1bbf8be0f55fa0c74508136389
Eva Morrison <eva.morrison33@ku.edu>
2022-12-04 19:05:12 -0600

    create streaks page

---
+++ frontend/src/pages/Streaks.js
@@ -0,0 +1,28 @@
+import ModuleCard from '../components/ModuleCard'
+import ModuleCard2 from '../components/ModuleCard2'
+import Grid from "@mui/material/Grid"
+import NavBar from '../components/NavBar'
+import '../App.css';
+import Clock from '../components/Clock'
+//import Image from '../images/BirdBackground.jpg'
+import Image from '../images/birdBackground2.jpg'
+
+const Streaks = () => {
+    return(
+        <div style={{ backgroundImage: `url(${Image})`, 
+        backgroundSize: "cover" }}>
+            <NavBar></NavBar>
+            <Clock/>
+            <Grid container spacing={12}>
+                <Grid  item xs={12} marginX = {5} marginBottom = {5}>
+                    <Grid container spacing={6}>
+                        <ModuleCard/>
+                        <ModuleCard2/>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default Streaks;
