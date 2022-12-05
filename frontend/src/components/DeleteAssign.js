94ecc4fe9f336b4dd2fbb39d78fbf64435803b6c
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-12-04 18:05:58 -0600

    Add files via upload

---
+++ frontend/src/components/DeleteAssign.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DeleteIcon from '@mui/icons-material/Delete';
+export default function BasicForm() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div >
+      <Button onClick={handleClickOpen} variant="outlined" sx={{backgroundColor: "cornsilk", fill: "blue", color:"Black", }}>
+      <DeleteIcon sx={{fontSize: "large", color: "red"}}/>
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+            Delete any assingment on page, future sprint
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Save</Button>
+        </DialogActions>
+      </Dialog>
+      
+    </div>
+  );
+}
