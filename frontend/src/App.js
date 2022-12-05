466c2c7299f1775adcf149b1562adc0fb3405b41
b185j406 <78661382+b185j406@users.noreply.github.com>
2022-12-04 16:18:48 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Health from './pages/Health'
 import Travel from './pages/Travel'
 import Social from './pages/Social'
-import Academic from './pages/Academic'
+import Academics from './pages/Academic'
 import PasswordReset from './pages/PasswordReset'
 function App() {
   const [currentUser, setCurrentUser] = useState('')
@@ -27,7 +27,7 @@ function App() {
           <Route path = '/travel' element={<Travel/>}/>
           <Route path = '/social' element={<Social/>}/>
           <Route path = '/template' element={<Template/>}/>
-          <Route path = '/academic' element={<Academic/>}/>
+          <Route path = '/academics' element={<Academics/>}/>
           <Route path = '/streaks' element={<Template/>}/>
           <Route path = '/home' element = {<Homepage/>}/>
         </Routes>
