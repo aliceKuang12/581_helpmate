53b2d19e2ed62f3deaf15d205c6e1963ad61042e
Alice Kuang <yuchun.kuang@gmail.com>
2023-01-23 17:21:30 -0600

    Revert "Add AWS Cognito (authentication)"
    This reverts commit 59b25274409926d93d8f948889046fa0fa1c0d1a.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,17 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
-  },
-  "auth": {
-    "helpmate1e9c404d2": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
   }
 }
