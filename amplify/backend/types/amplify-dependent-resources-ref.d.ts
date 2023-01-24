e7b3b6f3154bea11bbb3aae8993ce4e18289806c
Alice Kuang <yuchun.kuang@gmail.com>
2023-01-23 19:51:23 -0600

    Delete Amplify files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "helpmate1": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
