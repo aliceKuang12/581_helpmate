89b106afa7794c6a9650637cd67c3ef6309832dd
Alice Kuang <yuchun.kuang@gmail.com>
2023-01-23 19:48:12 -0600

    Revert "Successfully link aws amplify"
    This reverts commit a9ff3f03d49db88212bda55b7481d94a8ab6de6c.
    
    # Conflicts:
    #	frontend/src/App.js
    #	src/graphql/mutations.js
    #	src/graphql/schema.json
    #	src/graphql/subscriptions.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
 
@@ -53,4 +54,55 @@ return (
   </Container>
 );
 }
+=======
+import './App.css';
+import { useState } from "react";
+import axios from 'axios';
+import Homepage from './pages/Homepage'
+import Login from './pages/Login'
+import Template from './pages/Template'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Health from './pages/Health'
+import Travel from './pages/Travel'
+import Social from './pages/Social'
+import Academics from './pages/Academic'
+import Streaks from './pages/Streaks'
+import PasswordReset from './pages/PasswordReset'
+import { AuthProvider } from './context/AuthContext';
+import Container from '@mui/material/Container'
+import ForgotPassword from './pages/ForgotPassword';
+
+function App() {
+  const [currentUser, setCurrentUser] = useState('')
+
+  return (
+    <Container>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route 
+              path = '/' 
+              element={<Homepage user={currentUser}/>}
+            />
+            <Route 
+              path = '/login' 
+              element={<Login setCurrentUser={setCurrentUser}/>}
+            />
+            <Route path = '/health' element={<Health setCurrentUser={setCurrentUser}/>} />
+            <Route path = '/travel' element={<Travel setCurrentUser={setCurrentUser}/>}/>
+            <Route path = '/social' element={<Social setCurrentUser={setCurrentUser}/>}/>
+            <Route path = '/template' element={<Template/>}/>
+            <Route path = '/academics' element={<Academics setCurrentUser={setCurrentUser}/>}/>
+            <Route path = '/streaks' element={<Streaks setCurrentUser={setCurrentUser}/>}/>
+            <Route path = '/home' element = {<Homepage setCurrentUser={setCurrentUser}/>}/>
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path = '/reset' element={<PasswordReset/>}/>
+          </Routes>c
+        </AuthProvider>
+      </Router>
+    </Container>
+  );
+}
+
+>>>>>>> parent of a9ff3f0 (Successfully link aws amplify)
 export default App
