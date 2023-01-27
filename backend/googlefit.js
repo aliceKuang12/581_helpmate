e74f7e7ef28950f39088cef83b2e45842a6bc174
Alice Kuang <yuchun.kuang@gmail.com>
2023-01-27 10:05:57 -0600

    Googlefit- working steps and sleep data

--- backend/googlefit.js
+++ backend/googlefit.js
@@ -1,35 +1,63 @@
-
 import express from "express";
+import session from "express-session";
+const app = express();
 import cors from "cors"
 import { google } from "googleapis"
 import request from "request"
 import urlParse from "url-parse"
 import queryParse from "query-string"
 import bodyParser from "body-parser";
-import axios from "axios"
-import sql from "./app/models/db.js"
-import { DATE } from "sequelize";
+import axios from "axios";
 
-const app = express();
+//https://expressjs.com/en/guide/routing.html
+const router1 = express.Router();
+const router2 = express.Router();
+const router3 = express.Router();
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extende: true }));
+app.use(express.static('frontend'))
+app.use(express.json()); // parse requests of content-type - application/json
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/json
-app.use(express.json()); /* bodyParser.json() is deprecated */
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "sessions",
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      sameSite: "none",
+      // httpOnly: false,
+      secure: false,
+    },
+  })
+);
 
-// simple route https://youtu.be/-yH1ZnZBWyU
-app.get("/getUrl", (req, res) => {
-
-  const oauth2Client = new google.auth.OAuth2(
+function getOauth2Client(url) {
+  return new google.auth.OAuth2(
     //client-id,
     //client-secret,
-    //link to redirect
-    "http://localhost:5000/steps"
+    //link to redirect 
+    url
   );
+}
 
-  const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"];
+// Oauth2 playground / google fit sdk tutorial: https://www.youtube.com/watch?v=YMRhBPGLgmE
+// simple route https://youtu.be/-yH1ZnZBWyU
+router1.get("/getUrl", (req, res) => {
+
+  // console.log("router1 working");
+  const oauth2Client = getOauth2Client("http://localhost:5000/steps");
+
+  const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid",
+                  ];
 
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
@@ -46,68 +74,254 @@ app.get("/getUrl", (req, res) => {
     res.send({ url });
   });
 
-
 });
 
-app.get("/getUrl", async (req, res) => {
+
+
+router1.get("/steps", async (req, res) => {
   const queryURL = new urlParse(req.url);
   const code = queryParse.parse(queryURL.query).code;
-  const oauth2Client = new google.auth.OAuth2(
-    //client-id,
-    //client-secretr,
-    //link to redirect
-    "http://localhost:5000/steps"
-  );
-
+  const oauth2Client = getOauth2Client("http://localhost:5000/steps");
   const tokens = await oauth2Client.getToken(code);
-  res.send("Hello");
+  res.send("localhost:5000/steps served");
 
+  console.log('//------------------------------------------------')
   let stepArray = []
-
   try {
-      const result = await axios({
-        method: "POST",
-        headers: {
-          authorization: "Bearer " + tokens.tokens.access_token
-        },
-        "Content-Type": "application/json",
-        url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
-        data: {
-          aggregateBy: [
-            {
-              dataTypeName: "com.google.step_count.delta",
-              dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
-            }
-          ],
-          bucketByTime: { durationMillis: 86400000 },
-          startTimeMillis: Date.now() - 604800000,
-          endTimeMillis: Date.now(),
-        },
-      });
-   
-      //console.log(result);
-      stepArray = result.data.bucket;
-  } catch(e) {
+    const result = await axios({
+      method: "POST",
+      headers: {
+        authorization: "Bearer " + tokens.tokens.access_token
+      },
+      "Content-Type": "application/json",
+      url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
+      data: {
+        aggregateBy: [
+          {
+            dataTypeName: "com.google.step_count.delta",
+            dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+          }
+        ],
+        bucketByTime: { durationMillis: 86400000 }, // 86400000 - 1 day
+        startTimeMillis: Date.now() - 604800000, // 604800000 - 1week
+        endTimeMillis: Date.now(),
+      },
+    });
+
+    //console.log(result);
+    stepArray = result.data.bucket;
+  } catch (e) {
     console.log("Error 1: " + e);
   }
 
-  try{
-    for(const dataSet of stepArray) {
-     // console.log(dataSet);
-      for(const points of dataSet.dataset) {
-       // console.log(points);
-        for(const steps of points.point){
+  try {
+    for (const dataSet of stepArray) {
+      // console.log(dataSet);
+      for (const points of dataSet.dataset) {
+        // console.log(points);
+        for (const steps of points.point) {
           console.log(steps.value);
         }
       }
     }
-  } catch(e) {
+  } catch (e) {
     console.log("Error 2: " + e);
   }
 });
+//--------------------------------------------------------------------------------
+
+// res.cookie('Steps', steps.value).send('cookie set'); //new line
+
+// simple route https://youtu.be/-yH1ZnZBWyU
+router2.get("/getUrl2", (req, res) => {
+  const oauth2Client = getOauth2Client("http://localhost:5000/sleep");
+  const scopes = ["https://www.googleapis.com/auth/fitness.sleep.read profile email openid"];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+    state: JSON.stringify({
+      callbackUrl: req.body.callbackUrl,
+      userId: req.body.userId
+    })
+  });
+
+  request(url, (err, response, body) => {
+    console.log("error: ", err);
+    console.log("statusCode: ", response && response.statusCode);
+    res.send({ url });
+  });
+
+});
+
+//https://developers.google.com/fit/scenarios/write-sleep-data
+// https://developers.google.com/fit/scenarios/read-sleep-data
+router2.all("/sleep", async (req, res) => {
+  const queryURL = new urlParse(req.url);
+  const code = queryParse.parse(queryURL.query).code;
+  const oauth2Client = getOauth2Client("http://localhost:5000/sleep");
+  const tokens = await oauth2Client.getToken(code);
+  console.log(tokens);
+  res.send("server /sleep online");
+
+  let sessionData = []
+  try {
+    const result = await axios({
+      method: "GET",
+      headers: {
+        authorization: "Bearer " + tokens.tokens.access_token
+      },
+      "Content-Type": "application/json",
+      url: `https://www.googleapis.com/fitness/v1/users/me/sessions?activityType=72`,
+    });
+    sessionData = result.data.session;
+  } catch (e) {
+    console.log("Error HTTP GET: " + e);
+  }
+
+  try {
+    console.log('SLEEP session data: ');
+    console.log(sessionData);
+  }  catch (e) {
+    console.log(e);
+  }
+
+   console.log('//------------------------------------------------')
+  let stepArray = []
+  try {
+    const result = await axios({
+      method: "POST",
+      headers: {
+        authorization: "Bearer " + tokens.tokens.access_token
+      },
+      "Content-Type": "application/json",
+      url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
+      data: {
+        aggregateBy: [
+          {
+            dataTypeName: "com.google.step_count.delta",
+            dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+          }
+        ],
+        bucketByTime: { durationMillis: 86400000 }, // 86400000 - 1 day
+        startTimeMillis: Date.now() - 604800000, // 604800000 - 1week
+        endTimeMillis: Date.now(),
+      },
+    });
+
+    //console.log(result);
+    stepArray = result.data.bucket;
+  } catch (e) {
+    console.log("Error 1: " + e);
+  }
+
+  try {
+    for (const dataSet of stepArray) {
+      // console.log(dataSet);
+      for (const points of dataSet.dataset) {
+        // console.log(points);
+        for (const steps of points.point) {
+          console.log(steps.value);
+        }
+      }
+    }
+  } catch (e) {
+    console.log("Error 2: " + e);
+  }
+
+});
+//--------------------------------------------------------------------------------------
+router3.get("/getUrl3", (req, res) => {
+  const oauth2Client = getOauth2Client("http://localhost:5000/activity");
+  const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"];
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+    state: JSON.stringify({
+      callbackUrl: req.body.callbackUrl,
+      userId: req.body.userId
+    })
+  });
+
+  request(url, (err, response, body) => {
+    console.log("error: ", err);
+    console.log("statusCode: ", response && response.statusCode);
+    res.send({ url });
+  });
+
+});
+
+router3.get("/activity", async (req, res) => {
+  // console.log("router2 working");
+  const queryURL = new urlParse(req.url);
+  const code = queryParse.parse(queryURL.query).code;
+  const oauth2Client = getOauth2Client("http://localhost:5000/activity");
+  const tokens = await oauth2Client.getToken(code);
+  // console.log(tokens);
+  res.send("localhost:5000/activity");
+
+  let stepArray = []
+
+  try {
+    const result = await axios({
+      method: "POST",
+      headers: {
+        authorization: "Bearer " + tokens.tokens.access_token
+      },
+      "Content-Type": "application/json",
+      url: `https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate`,
+      data: {
+        aggregateBy: [
+          {
+            dataTypeName: "com.google.activity.segment",
+            dataSourceId: "derived:com.google.activity.segment:com.google.android.apps.fitness:session_activity_segment"
+          }
+        ],
+        bucketByTime: { durationMillis: 86400000 }, // 86400000 - 1 day
+        startTimeMillis: Date.now() - 604800000, // 604800000 - 1week
+        endTimeMillis: Date.now(),
+      },
+    });
+
+    //console.log(result);
+    stepArray = result.data.bucket;
+  } catch (e) {
+    console.log("Error 1: " + e);
+  }
+
+  try {
+     for (const dataSet of stepArray) {
+       console.log(dataSet);
+       for (const points of dataSet.dataset) {
+         console.log(points);
+    //     for (const steps of points.point) {
+    //       console.log(steps.value);
+    //       // res.redirect('http://localhost:3000/health')
+    //     }
+      }
+    }
+  } catch (e) {
+    console.log("Error 2: " + e);
+  }
+  
+  //res.sendFile("../frontend/src/components/HealthCard2.js");
+});
+
+app.use(router1);
+app.use(router2);
+app.use(router3);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+export default {
+  router1
+}
+
+export {
+  router2, router3
+}
+
