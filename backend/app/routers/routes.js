48b3621fd68f72dea1d409998c7c36005a52da60
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-01-29 11:59:43 -0600

    edit app express

--- backend/app/routers/routes.js
+++ backend/app/routers/routes.js
@@ -1,13 +1,6 @@
-module.exports = app => {
-    const tutorials = require("../controllers/controller.js");
-  
-    var router = require("express").Router();
-  
-    // // Create a new Tutorial
-    // router.post("/", tutorials.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
-  
- 
-  };
+import * as user from "../controllers/userController.js";
+import app from "../../server.js"
+
+app.post("/users", user.createUser);
+
+app.get("/users", user.findAll);
