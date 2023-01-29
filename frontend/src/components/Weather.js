922012f3a981b2b4e27c1c8e90faeb362e64b129
Eva Morrison <eva.morrison33@ku.edu>
2023-01-29 13:39:34 -0600

    create weather component from api

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -1,28 +1,59 @@
+import {useEffect, useState} from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 const Weather = () => {
-    fetch("http://api.weatherapi.com/v1/current.json?key=7f716f37a5c243009af191541232501&q=Lawrence, KS&aqi=no", {
-    "method": "GET",
-    "headers": {
-    }
-   })
-  .then(response => {
-    return processWeatherData(response);
+  const [weather, setWeather] = useState([])
+  const [location, setLocation] = useState([])
 
-  })
-  .catch(err => {
-    console.error(err);
-  });
+  const getWeather = async () => {
+    await fetch("http://api.weatherapi.com/v1/current.json?key=7f716f37a5c243009af191541232501&q=Lawrence, KS&aqi=no", {
+      "method": "GET",
+      "headers": {}
+      })
+      .then((response) => {
+          return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+          setWeather(data.current);
+          setLocation(data.location);
+      })
+      .catch(err => {
+          console.error(err);
+      });
+    };
+
+    useEffect(() => {
+      getWeather()
+    }, [])
+  
+  
+      
+      // Call the API
+  /*fetch("http://api.weatherapi.com/v1/current.json?key=7f716f37a5c243009af191541232501&q=Lawrence, KS&aqi=no").then(function (response) {
+      if (response.ok) {
+          return response.json();
+      } else {
+          return Promise.reject(response);
+      }
+      }).then(function (data) {
+      // Store the data to a variable
+      weather_data = data;
+  });*/
+   return(
+    <Container sx ={{marginRight: 2, marginY: .5}}> 
+      <Typography
+      variant = "h6"
+      component = "h6"
+      position = "relative"
+      align = 'right'
+      color = 'white'
+      >
+          {location.name}, {location.region}: {weather.temp_f}{'\u00B0'}F {weather.condition.text}
+      </Typography>
+  </Container>
+   )
 }
 
-
-
-
-function processWeatherData(response) {
-  
-    var location=response.resolvedAddress;
-    var days=response.days;
-    console.log("Location: "+location);
-    for (var i=0;i<days.length;i++) {
-      console.log(days[i].datetime+": tempmax="+days[i].tempmax+", tempmin="+days[i].tempmin);
-    }
-  }
+export default Weather;
