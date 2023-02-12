e4a24a75c7fc6c3e1dd4e668df1a570d2a385784
Eva Morrison <eva.morrison33@ku.edu>
2023-02-11 23:02:28 -0600

    connect backend to frontend

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -6,13 +6,13 @@ const dbConfig = {
     password: "CAT_33k1w1",
     database: "helpmate"
   },
-  production: {
-    host: "database-1.cdronrai97vl.us-east-1.rds.amazonaws.com",
-    port:"3306",
-    user: "helpMate",
-    password: "Thai21200126.",
-    database:"helpMate"
-  }
+  // production: {
+  //   host: "database-1.cdronrai97vl.us-east-1.rds.amazonaws.com",
+  //   port:"3306",
+  //   user: "helpMate",
+  //   password: "Thai21200126.",
+  //   database:"helpMate"
+  // }
 };
 
 export default dbConfig;
