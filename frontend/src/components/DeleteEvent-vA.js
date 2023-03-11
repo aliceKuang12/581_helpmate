af49e657c50ad5a9cc9771eb16207f830ed5b94b
Eva Morrison <eva.morrison33@ku.edu>
2023-03-10 20:14:48 -0600

    create delete eevent form for travel

--- frontend/src/components/DeleteEvent-vA.js
+++ frontend/src/components/DeleteEvent-vA.js
@@ -4,6 +4,11 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DeleteIcon from '@mui/icons-material/Delete';
+
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+
 export default function BasicForm() {
   const [open, setOpen] = React.useState(false);
 
@@ -23,7 +28,74 @@ export default function BasicForm() {
 
       <Dialog open={open} onClose={handleClose}>
         <DialogContent>
-            Delete any event on page, future sprint
+            <br/>
+            <Typography sx={{textAlign: 'center'}}>
+              Delete Event  
+            </Typography> 
+            <br/> 
+            <Grid container  spacing={2}
+            //justifyContent="center"
+            alignItems="center"
+          >  
+            <Grid item xs={2}>
+            <Typography sx={{fontSize: 16, textAlign: 'left', marginY: 1, padding: 2}}>
+              Title:
+            </Typography>  
+            </Grid>
+            <Grid item xs={9.5}>
+              <TextField
+                id="title"
+                label="event title"
+                variant="outlined"
+                //onChange={e => handleChange(e.target.value, 'title')}
+                //value={title}
+                fullWidth
+              /> 
+            </Grid>
+
+            <Grid item xs={2}>
+            <Typography sx={{fontSize: 16, textAlign: 'left', marginY: 1, padding: 2}}>
+            Category:
+            </Typography>  
+            </Grid>
+            <Grid item xs={9.5}>
+              <TextField
+                id="title"
+                label="event category"
+                variant="outlined"
+                //onChange={e => handleChange(e.target.value, 'category')}
+                //value={category}
+                fullWidth
+              /> 
+            </Grid>
+              
+            <Grid item xs={2}>
+            <Typography sx={{fontSize: 16, textAlign: 'left', marginY: 1, padding: 2}}>
+              Date:
+            </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <TextField
+              id="date"
+              type="date"
+              variant="outlined"
+              //onChange={e => handleChange(e.target.value, 'date')}
+              //value={date}
+              fullWidth
+              />  
+            </Grid>
+            <Grid item xs={4.5}>
+            <TextField
+                id="eventTime"
+                type="time"
+                variant="outlined"
+                //onChange={e => handleChange(e.target.value, 'eventTime')}
+                //value={eventTime}
+                fullWidth
+              />
+            </Grid> 
+            
+            </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
