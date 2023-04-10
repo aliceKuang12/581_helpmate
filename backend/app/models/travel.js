2c309238095ef61247033db06468faf4c079a18a
Minh Huyen Nguyen <huyennm2002@gmail.com>
2023-04-10 10:34:44 -0500

    update card in homepage

--- backend/app/models/travel.js
+++ backend/app/models/travel.js
@@ -44,7 +44,7 @@ Travel.show = (user_id, result) => {
     let query = `SELECT * from travel`
     if (user_id) {
         query = `SELECT * from travel WHERE userId = ? AND eventTime >= NOW() ORDER BY eventTime ASC`
-    }
+}
     sql.query(query, [user_id], (err, res) => {
         if (err) {
             result(err,null);
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -13,6 +13,7 @@ import Weather from '../components/Weather'
 import modules from "./modules.json"
 import Image from '../images/homebackground2.jpeg'
 import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
 
 
 const Homepage = (props) => {
@@ -21,44 +22,45 @@ const Homepage = (props) => {
     const [Travel, setTravel] = useState([]);
     const [Health, setHealth] = useState([]);
     const [user1, setUser1] = useState([]);
-
-
+    const { currentUser } = useAuth();
+    const user = JSON.parse(currentUser);
+    console.log(user.id);
     // https://stackoverflow.com/questions/52669596/promise-all-with-axios
     useEffect(() => {
         const fetchAllTables = async () => {
             // await axios.get(url)// + localStorage.getItem("email"))
-            let URL1 = "http://localhost:3003/academics/"
-            let URL2 = "http://localhost:3003/health/"
-            let URL3 = "http://localhost:3003/travel/"
-            let URL4 = "http://localhost:3003/social/"
-            let URL5 = "http://localhost:3003/user/"
-
-            const promise1 = await axios.get(URL1 + localStorage.getItem("email")).then(res => {
-                setAcademic((res.data));
+            let URL1 = "http://localhost:3003/academics"
+            let URL2 = "http://localhost:3003/healths"
+            let URL3 = "http://localhost:3003/travels"
+            let URL4 = "http://localhost:3003/social"
+            let URL5 = "http://localhost:3003/user"
+        
+            const promise1 = await axios.get(URL1, {params: {user_id: user.id}}).then(res => {
+                setAcademic((res.data.slice(0, 3)));
                 console.log(res.data);
             })
                 .catch(err => {
                     console.log(err)
                 });
 
-            const promise2 = await axios.get(URL2 + localStorage.getItem("email")).then(res => {
-                setHealth((res.data));
+            const promise2 = await axios.get(URL2, {params: {user_id: user.id}}).then(res => {
+                setHealth((res.data.slice(0, 3)));
                 console.log(res.data);
             })
                 .catch(err => {
                     console.log(err)
                 });
 
-            const promise3 = await axios.get(URL3 + localStorage.getItem("email")).then(res => {
-                setTravel((res.data));
+            const promise3 = await axios.get(URL3, {params: {user_id: user.id}}).then(res => {
+                setTravel((res.data.slice(0, 3)));
                 console.log(res.data);
             })
                 .catch(err => {
                     console.log(err)
                 });
 
-            const promise4 = await axios.get(URL4 + localStorage.getItem("email")).then(res => {
-                setSocial((res.data));
+            const promise4 = await axios.get(URL4, {params: {user_id: user.id}}).then(res => {
+                setSocial((res.data.slice(0, 3)));
                 console.log(res.data);
             })
                 .catch(err => {
@@ -66,7 +68,7 @@ const Homepage = (props) => {
                 });
 
             const promise5 = await axios.get(URL5 + localStorage.getItem("email")).then(res => {
-                setUser1((res.data));
+                setUser1((res.data.slice(0, 3)));
                 console.log(res.data);
             })
                 .catch(err => {
